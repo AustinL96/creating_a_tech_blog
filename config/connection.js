0b3d41566e04ab4beb4e4f3424f20424006ba373
Austin Lewis <austinlewis165@gmail.com>
2023-04-21 11:30:11 -0400

    new heroku fix

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 const isProduction = process.env.PORT;
 
-const connection = isProduction ? process.env.DB_CONNECTION_STRING : new Sequelize(
+const connection = isProduction ? new Sequelize (process.env.DB_CONNECTION_STRING) : new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
